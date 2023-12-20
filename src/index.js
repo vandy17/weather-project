45ede8622e6525f5062ec05a9c08f96522835977
Vandana gupta <vandanaguptauk@gmail.com>
2023-12-20 22:13:03 +0530

    update js file

--- src/index.js
+++ src/index.js
@@ -59,3 +59,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
